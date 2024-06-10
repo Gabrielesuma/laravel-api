8a0f9d2efa8dafd0f71d72cad3c89763a84af416
Gabrielesuma <gabrielesuma139@gmail.com>
2024-06-10 17:40:52 +0200

    milestone 1 e 2 api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+        //dd($projects);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show($slug){
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+        if($project){
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'post not found'
+            ]);
+        }
+    }
+}
