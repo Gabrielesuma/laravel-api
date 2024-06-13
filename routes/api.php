2b03af8b7dba3587dac223e7d84d8ea358eefcdf
Gabrielesuma <gabrielesuma139@gmail.com>
2024-06-13 16:40:15 +0200

    controller model migration lead

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
@@ -19,6 +20,7 @@ use App\Http\Controllers\Api\TypeController;
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
 Route::get('types', [TypeController::class, 'index']);
+Route::post('contacts', [LeadController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
